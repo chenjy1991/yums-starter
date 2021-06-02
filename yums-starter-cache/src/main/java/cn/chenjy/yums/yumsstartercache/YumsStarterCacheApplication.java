32c0ef533a4c3f9d32e9309f7b5b824ac23410b3
chenjy <chenjy@chenj.cn>
2021-06-02 16:24:17 +0800

    添加cache模块

---
+++ yums-starter-cache/src/main/java/cn/chenjy/yums/yumsstartercache/YumsStarterCacheApplication.java
@@ -0,0 +1,13 @@
+package cn.chenjy.yums.yumsstartercache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumsStarterCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumsStarterCacheApplication.class, args);
+    }
+
+}
