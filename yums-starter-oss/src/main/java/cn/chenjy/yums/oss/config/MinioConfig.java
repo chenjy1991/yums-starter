ec4230a28a996bd4446027326994c0a6a28525c0
chenjy <chenjy@chenj.cn>
2021-06-08 02:47:34 +0800

    starter-oss发布了1.0.0正式版

---
+++ yums-starter-oss/src/main/java/cn/chenjy/yums/oss/config/MinioConfig.java
@@ -0,0 +1,15 @@
+package cn.chenjy.yums.oss.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ChenJY
+ * @create 2021/6/8 12:01 上午
+ * @DESCRIPTION
+ */
+public class MinioConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(MinioConfig.class);
+    private static final String TAG = "MinioConfig";
+
+}
